40b6e7956a43a77f3286aa125751ff0034749348
Мария МД <mahadrum@yandex.ru>
2026-03-03 10:18:37 +0300

    красиво отобразили параметры товара спередачей парметка как модели

--- OnLineShop2026/Controllers/ProductController.cs
+++ OnLineShop2026/Controllers/ProductController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
 
 namespace OnLineShop2026.Controllers
 {
     public class ProductController : Controller
     {
+
         public IActionResult Index()
         {
-            return View();
+            Product pie = new Product("Пирожок", "с котятами", 50);
+            return View(pie);
         }
+
+        //public IActionResult Index()
+        //{
+        //    Product pie = new Product("Пирожок", "с котятами", 50);
+        //    ViewData["pie"] = pie;
+        //    return View();
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    Product pie = new Product("Пирожок", "с котятами", 50);
+        //    ViewBag.Pie = pie;
+        //    return View();
+        //}
+
+        //public string Index()
+        //{
+        //    Product pie = new Product("Пирожок", "с котятами", 50);
+        //    return pie.ToString();
+        //}
     }
 }
