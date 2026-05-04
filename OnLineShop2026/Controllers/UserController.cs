e27fa5b06469884c03de970f266e326619e143df
Marina <marina.khodova.06@mail.ru>
2026-05-04 19:05:47 +0300

    Контроллер и представление,  отображающее пользователей в виде таблицы

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Data;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserRepository userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+        public IActionResult Catalog()
+        {
+            var users = userRepository.GetAll();
+            return View(users);
+
+            //User user = new User("Пользователь", "Первый", "example@email");
+            ////var user = userRepository.TryGetById(id);
+            //return View(user);
+        }
+    }
+}
