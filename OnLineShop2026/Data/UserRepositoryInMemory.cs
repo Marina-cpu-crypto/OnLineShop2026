1e0d9dce2bed41b6cfcd673fe86723678c001b38
Marina <marina.khodova.06@mail.ru>
2026-05-04 18:51:52 +0300

    класс UserRepository

---
+++ OnLineShop2026/Data/UserRepositoryInMemory.cs
@@ -0,0 +1,24 @@
+﻿using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Data
+{
+    public class UserRepositoryInMemory : IUserRepository
+    {
+        private static List<User> users = new List<User>()
+        {
+            new User("Первый", "Поперечный", "first@mail.ru"),
+            new User("Второй", "Вдольный","second@mail.ru"),
+            new User("Третий", "Треугольный","third@mail.ru")
+        };
+
+        public List<User> GetAll()
+        { 
+            return users;
+        }
+
+        public User? TryGetById(Guid id)
+        {
+            return users.FirstOrDefault(user => user.UserId == id);
+        }
+    }
+}
